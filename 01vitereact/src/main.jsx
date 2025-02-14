054728034e5693b2438ccfbf9f425579ed8d72ec
Abhishek Raushan <abhishekraushan002@gmail.com>
2025-02-14 23:58:33 +0530

    Day 2 | Creating our Components + Getting to know about fragment

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Chai from './Chai.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
+    <Chai />
     <App />
-  </StrictMode>,
+    <h2>Using fragment</h2>
+  </>,
 )

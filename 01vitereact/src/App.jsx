b9a3aafe3cb5e47fd2f43ea373f92787da89d6e7
Abhishek Raushan <abhishekraushan002@gmail.com>
2025-02-16 19:02:18 +0530

    Day 4 | Render funcation changes | Variable creation and usage

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,9 @@
 function App() {
 
+  const username = "By super Intelligent Hitesh"
   return (
     <>
-      <h1>Chai aur React with Vite</h1>
+      <h1>Chai aur React with Vite {username}</h1>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,11 +2,44 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Chai from './Chai.jsx'
 import App from './App.jsx'
+import React from 'react'
+
+// Rendering html using function.
+/*
+function MyApp(){
+  return(
+    <div>
+      <h1>Chai  with HC</h1>
+    </div>
+  )
+}*/
+
+/* 
+  // This is not working because we are giving convension according to our thinking.
+  const ReactElement = {
+  type: 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  children: 'Click me to visit google'
+} */
+
+/*
+  //how react creates an elements
+  const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visite to google'
+  )*/
+
+    /*
+const anotherElement = (
+  <a href = "https://google.com" target='_blank'>Visite google</a>
+)*/
 
 createRoot(document.getElementById('root')).render(
-  <>
-    <Chai />
-    <App />
-    <h2>Using fragment</h2>
-  </>,
-)
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
